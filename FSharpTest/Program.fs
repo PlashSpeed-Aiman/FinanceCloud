316dff1d77f2cb485d978a69fa36720ad872c597
PlashSpeed-Aiman <maiman.ar@live.iium.edu.my>
2022-11-23 13:35:36 +0800

    Updated functionality

--- FSharpTest/Program.fs
+++ FSharpTest/Program.fs
@@ -18,8 +18,12 @@ type FinanceCategories =
     | Lifestyle
     | Utilities
     | Others
+type ProgOptions =
+    |AddEntry
+    |ViewEntries
+    |ExitProg
 type FinanceEntry =
-    {EntryName:string;Amount:double;EntryDate:DateTime;Category:FinanceCategories}
+    {EntryName:string;Amount:double;EntryDate:string;EntryMonth:int;EntryYear:int;Category:FinanceCategories}
 
 let getAmount item = item.Amount
 
@@ -36,17 +40,57 @@ let SendToDatabase  (x:SQLiteConnection) (item:FinanceEntry) :unit  =
     let reader =  new SQLiteCommand(insertSql,x) |> fun cmd -> cmd.ExecuteNonQuery()
     ()
     
+let ProgramSelect () =
+    Console.WriteLine "Finance App\n1.Add Entry\n2.View Monthly Entries"
+    let value = Console.ReadLine()
+    let helperfunc value =
+        match value with
+            |"1" -> AddEntry
+            |"2" -> ViewEntries
+            |"3" -> ExitProg
+    let res = helperfunc value
+    res
+
+let AddEntryFunc conn =
+    Console.WriteLine "Insert Entry Name"
+    let entryname = Console.ReadLine()
+    Console.WriteLine "Insert Amount"
+    let amount = Console.ReadLine()
+    Console.WriteLine "Category\n1.Food\n2.Lifestyle\n3.Utilities\n4.Others"
+    let category_val = Console.ReadLine()
+    let helperfunc category_val =
+        match category_val with
+            |"1" -> Food
+            |"2" -> Lifestyle
+            |"3" -> Utilities
+            |"4"|_ -> Others
+    let category_val_DU = helperfunc category_val
+    let entry_item  = {EntryName=entryname;Amount= (Double.Parse amount); EntryDate =  DateTime.Today.ToShortDateString();EntryMonth= DateTime.Today.Month;EntryYear= DateTime.Today.Year;Category=category_val_DU }
+    SendToDatabase conn entry_item
+    ()
+
 [<EntryPoint>]
 let main argv =
     let conn = db_connect()
     let partial_send = SendToDatabase conn
-    let newitem  = {EntryName="1Sauce";Amount=10.50;EntryDate = DateTime.Today;Category= Food}
-    let newitem2 = {EntryName="2Sauce";Amount=21.50;EntryDate = DateTime.Today;Category= Lifestyle}
-    let newitem3 = {EntryName="3Sauce";Amount=20.50;EntryDate = DateTime.Today; Category = Others}
+    let newitem  = { EntryName="1Sauce"
+                     Amount=10.50
+                     EntryDate = DateTime.Today.ToShortDateString()
+                     EntryMonth=DateTime.Today.Month
+                     EntryYear=DateTime.Today.Year
+                     Category = Food }
+    let mutable app_condition = false
+    while not app_condition do 
+        let res = ProgramSelect()
+        match res with
+            |AddEntry -> AddEntryFunc conn
+            |ViewEntries|ExitProg -> app_condition <- true
+        () 
+    
     (*
-    [newitem;newitem2;newitem3] |> List.map partial_send |> ignore 
+    let list1 = [newitem.Amount;newitem2.Amount;newitem3.Amount] 
     *)
-    let electionData = 
+    (*let electionData = 
         [ "Conservative", 306; "Labour", 258; 
                                 "Liberal Democrat", 57 ]
     let Bolivia = ["2004/05", 165.; "2005/06", 135.; "2006/07", 157.; "2007/08", 139.; "2008/09", 136.]
@@ -56,16 +100,16 @@ let main argv =
     let series = [ "bars"; "bars"; "bars"; "lines" ]
     let inputs = [ Bolivia; Ecuador; Madagascar; Average ]
     let chart2 =
-        inputs
-        |> Chart.Combo
+        list1
+        |> Chart.Bar
         |> Chart.WithOptions
             (Options(title = "Coffee Production",
                     series = [| for typ in series -> Series(typ) |]))
-        |> Chart.WithLabels ["Bolivia"; "Ecuador"; "Madagascar"; "Average"]
+        |> Chart.WithLabels ["Bolivia"]
         |> Chart.WithLegend true
         |> Chart.WithSize (600, 250)
-        |>Chart.Show
-    Console.WriteLine newitem3
+        |>Chart.Show*)
+    Console.WriteLine newitem
     0
 
 
